311fac81b94c08affdfbdd1eb8b119ea993bffe5
Douglas Soares <douglas.sl96@gmail.com>
2021-03-15 21:39:44 -0300

    feat: adds tls certificates

--- api/client/client.go
+++ api/client/client.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"encoding/gob"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"time"
 
 	naming "github.com/douglas-soares/rpc-quick/src/naming_service"
@@ -21,15 +24,26 @@ func main() {
 	n.StartClient(tlsConfN)
 	s, err := n.LookUp("Servidor")
 	fmt.Println(" testing naming:", s, err)
+	cert, err := tls.LoadX509KeyPair("../../cert.pem", "../../key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCert, err := ioutil.ReadFile("../../cert.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
 
 	tlsConf := &tls.Config{
-		InsecureSkipVerify: true,
-		NextProtos:         []string{"quic-echo-example"},
+		RootCAs:      caCertPool,
+		Certificates: []tls.Certificate{cert},
+		NextProtos:   []string{"quic-echo-example"},
 	}
 
 	proxy := rpc.NewClient(s.Addr, tlsConf, nil)
 	start := time.Now()
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 5000; i++ {
 		var resp int
 		err = proxy.Call(&resp, "fibonacci", 20)
 		if err != nil {
--- api/server/server.go
+++ api/server/server.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
-	"encoding/pem"
 	"fmt"
-	"math/big"
+	"io/ioutil"
+	"log"
 
 	naming "github.com/douglas-soares/rpc-quick/src/naming_service"
 	"github.com/douglas-soares/rpc-quick/src/rpc"
@@ -37,24 +35,24 @@ func fibonacci(n int) int {
 }
 
 func GenerateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	// cer, err := tls.LoadX509KeyPair("../../server.crt", "../../server.key")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	cert, err := tls.LoadX509KeyPair("../../cert.pem", "../../key.pem")
+	caCert, err := ioutil.ReadFile("../../cert.pem")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	tls := &tls.Config{
+		ClientCAs:    caCertPool,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{cert},
 		NextProtos:   []string{"quic-echo-example"},
 	}
+
+	tls.BuildNameToCertificate()
+	return tls
 }
